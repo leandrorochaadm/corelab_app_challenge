34f6663cfcc09b3b97a43eea9c0d200202ce52f8
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-08-29 19:28:33 -0300

    move title_widget

--- lib/application/core/widgets/widgets.dart
+++ lib/application/core/widgets/widgets.dart
@@ -2,3 +2,4 @@ export 'button_navigator_bar_custom.dart';
 export 'error_custom_widget.dart';
 export 'not_found_search_widget.dart';
 export 'search_bar_widget.dart';
+export 'title_widget.dart';
--- lib/application/pages/home/widgets/widgets.dart
+++ lib/application/pages/home/widgets/widgets.dart
@@ -2,4 +2,3 @@ export 'history_search_bar_widget.dart';
 export 'last_ads_widget.dart';
 export 'product_widget.dart';
 export 'products_widget.dart';
-export 'title_widget.dart';
