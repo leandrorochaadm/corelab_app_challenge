e3e8f74d674c117fb0afc72485612cd824a7deb4
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-08-28 21:59:34 -0300

    feat: historySearch to searching

--- lib/application/pages/home/widgets/history_search_bar_widget.dart
+++ lib/application/pages/home/widgets/history_search_bar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:corelab_app_challenge/application/pages/home/cubit/cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../../theme_custom.dart';
 
@@ -13,12 +14,13 @@ class HistorySearchBarWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final historics = state.historySearch;
+    final cubit = context.read<HomeCubit>();
     return ListView.builder(
       itemCount: historics.length,
       itemBuilder: (context, index) {
         final historic = historics[index];
         return ListTile(
-          onTap: () {},
+          onTap: () => cubit.searchProducts(nameProduct: historic),
           title: Text(historic, style: textStyleLabelItemList),
           leading: const Icon(Icons.history, color: grey06),
           dense: true,
