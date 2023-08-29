c08631e4c58962ef1b8bc6a68c2500bf13cb4f72
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-08-28 22:08:46 -0300

    feat: SearchBarWidget: button filter

--- lib/application/core/widgets/search_bar_widget.dart
+++ lib/application/core/widgets/search_bar_widget.dart
@@ -1,4 +1,5 @@
 import 'package:corelab_app_challenge/application/pages/home/cubit/cubit.dart';
+import 'package:corelab_app_challenge/theme_custom.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -21,43 +22,66 @@ class SearchBarWidget extends StatelessWidget {
       child: Column(
         children: [
           const SizedBox(height: 12),
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(8)),
-            child: Row(
-              children: [
-                const SizedBox(width: 12),
-                Expanded(
-                  child: TextField(
-                    controller: _searchController,
-                    onTap: () => cubit.searching(),
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                    ),
-                    decoration: const InputDecoration(
-                      hintText: 'Buscar',
-                      hintStyle: TextStyle(color: Color(0xFF94A6A9)),
-                      border: InputBorder.none,
-                    ),
-                    onChanged: (value) {
-                      // Perform search functionality here
-                    },
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 12),
+                      Expanded(
+                        child: TextField(
+                          controller: _searchController,
+                          onTap: () => cubit.searching(),
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          decoration: const InputDecoration(
+                            hintText: 'Buscar',
+                            hintStyle: TextStyle(color: Color(0xFF94A6A9)),
+                            border: InputBorder.none,
+                          ),
+                          onChanged: (value) {
+                            // Perform search functionality here
+                          },
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Visibility(
+                        visible: state.status != HomeStateStatus.searching,
+                        child: IconButton(
+                          onPressed: () => cubit.searchProducts(
+                              nameProduct: _searchController.text),
+                          icon: Icon(
+                            Icons.search,
+                            color: theme.primaryColor,
+                            size: 30,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 12),
+                    ],
                   ),
                 ),
-                const SizedBox(width: 8),
-                IconButton(
-                  onPressed: () =>
-                      cubit.searchProducts(nameProduct: _searchController.text),
-                  icon: Icon(
-                    Icons.search,
-                    color: theme.primaryColor,
-                    size: 30,
-                  ),
-                ),
-                const SizedBox(width: 12),
-              ],
-            ),
+              ),
+              Visibility(
+                visible: state.status == HomeStateStatus.searching,
+                child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Filtros',
+                      style: TextStyle(
+                        color: background,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 14,
+                      ),
+                    )),
+              )
+            ],
           ),
         ],
       ),
