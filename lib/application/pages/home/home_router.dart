6e4eca47b5a71ec6c413bee6fc5678562cadc1bd
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-08-29 13:26:30 -0300

    feat: home router add provider getProducts

--- lib/application/pages/home/home_router.dart
+++ lib/application/pages/home/home_router.dart
@@ -1,3 +1,6 @@
+import 'package:corelab_app_challenge/data/usecases/remote_get_products.dart';
+import 'package:corelab_app_challenge/domain/usecases/usecases.dart';
+import 'package:corelab_app_challenge/infra/rest_client/custom_dio.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -8,7 +11,16 @@ class HomeRouter {
   HomeRouter._();
 
   static Widget get page => MultiProvider(
-        providers: [Provider(create: (context) => HomeCubit()..lastAds())],
+        providers: [
+          Provider<CustomDio>(create: (context) => CustomDio()),
+          Provider<GetProducts>(
+              create: (context) =>
+                  RemoteGetProduct(dio: context.read<CustomDio>())),
+          Provider(
+              create: (context) =>
+                  HomeCubit(getProducts: context.read<GetProducts>())
+                    ..lastAds()),
+        ],
         child: const HomePage(),
       );
 }
